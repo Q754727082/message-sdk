ce04ea61cc4c958f8ceb5f31444b5c1a4f3dde22
Q754727082 <Q754727082@163.com>
2023-03-19 15:50:40 +0800

    消息处理SDK提交

---
+++ src/main/java/com/ohc/messagesdk/mapper/MqMessageHistoryMapper.java
@@ -0,0 +1,15 @@
+package com.ohc.messagesdk.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ohc.messagesdk.model.po.MqMessageHistory;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author ohc
+ */
+public interface MqMessageHistoryMapper extends BaseMapper<MqMessageHistory> {
+
+}
